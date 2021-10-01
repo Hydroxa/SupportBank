f2ed8669ac8851aac8470456f71ffcadf0ef363a
Hydroxa <alexgillott489@gmail.com>
2021-10-01 18:24:39 +0100

    Added XMLParsing, export function and refactored

--- dataParsers.js
+++ dataParsers.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const MalformedTransaction = require("./cMalformedTransaction");
 const logs = require("log4js").getLogger("parsing");
 const LogFormats = require("./cLogFormats");
-const XMLParse = require("fast-xml-parser");
 
 class CSVParser {
 	static parseTransactionsFromFile(filePath) {
@@ -16,11 +15,11 @@ class CSVParser {
 			if (rawTransactionsLines[i] === "") continue;
 
 			const transactionText = rawTransactionsLines[i];
-			let transaction = new Transaction(transactionText);
+			let transaction = Transaction.createFromString(transactionText);
 
 			if (transaction.isMalformed()) {
 				logs.debug(`Line ${i + 1} was malformed.`);
-				transaction = new MalformedTransaction(transactionText);
+				transaction = MalformedTransaction.createFromString(transactionText);
 			}
 			transactions.push(transaction);
 		}
@@ -36,7 +35,7 @@ class CSVParser {
 			const rawTransactionsText = this.removeHeader(rawTransactionFile.split("\n"));
 			return rawTransactionsText;
 		} catch (err) {
-			console.error("Could not read the file! Check .\\logs\\debug.logs for details");
+			console.error("Could not read the file! Check .\\logs\\latest.logs for details");
 			logs.error(LogFormats.formatError(err, "Failed to read file!"));
 			return "";
 		}
@@ -57,11 +56,11 @@ class JSONParser {
 			if (jsonTransactionsFile[i].length === 0) continue;
 
 			const transactionObject = jsonTransactionsFile[i];
-			let transaction = new Transaction(transactionObject);
+			let transaction = Transaction.createFromJSON(transactionObject);
 
 			if (transaction.isMalformed()) {
 				logs.debug(`Object ${i + 1} was malformed.`);
-				transaction = new MalformedTransaction(transactionObject);
+				transaction = MalformedTransaction.createFromJSON(transactionObject);
 			}
 			transactions.push(transaction);
 		}
@@ -74,28 +73,45 @@ class XMLParser {
 	static parseTransactionsFromFile(filePath) {
 		logs.warn("XML Parsing is not fully implemented yet! This call will result in an error");
 
-		let transactions = [];
+		const transactions = [];
 		try {
 			const transactionFileLines = this.readTransactionFileLines(filePath);
+			const OLEconversionRatio = 24 * 2600 * 1000;
 
 			let transactionBuilding = false;
 			let transaction;
 			for (let i = 0; i < transactionFileLines.length; i++) {
 				const line = transactionFileLines[i];
-				if (!line.contains("TransactionLine")) {
-					if (line.contains("<SupportTransaction")) {
-						if (transactionBuilding) throw Error(`Incomplete Transaction. Expected end at line ${i + 1}`);
-						else transactionBuilding = true;
-
-						transaction = new Transaction({ type: "XML" });
+				if (line.indexOf("<SupportTransaction") !== -1) {
+					if (transactionBuilding) {
+						throw Error(`Incomplete Transaction. Expected end at line ${i + 1}`);
+					} else {
+						transactionBuilding = true;
+					}
+					transaction = new Transaction();
+					transaction.date = new Date();
+					transaction.date.setTime((Number(line.split('"')[1]) - 25569) * OLEconversionRatio);
+				} else if (line.indexOf("</SupportTransaction>") !== -1) {
+					transactions.push(transaction);
+					transactionBuilding = false;
+				} else if (transactionBuilding) {
+					if (line.indexOf("<Description>") !== -1) {
+						transaction.reason = line.substring(line.indexOf(">") + 1, line.lastIndexOf("<"));
+					} else if (line.indexOf("<Value>") !== -1) {
+						transaction.amount = Number(line.substring(line.indexOf(">") + 1, line.lastIndexOf("<")));
+					} else if (line.indexOf("<From>") !== -1) {
+						transaction.from = line.substring(line.indexOf(">") + 1, line.lastIndexOf("<"));
+					} else if (line.indexOf("<To>") !== -1) {
+						transaction.to = line.substring(line.indexOf(">") + 1, line.lastIndexOf("<"));
 					}
 				}
 			}
 		} catch (err) {
-			console.error("Could not read the file! Check .\\logs\\debug.logs for details");
-			logs.error(LogFormats.formatError(err, "Failed to read file!"));
-			return transactions;
+			console.error("Could not read the file! Check .\\logs\\latest.logs for details");
+			logs.error(LogFormats.formatError(err, "Error when reading file!"));
 		}
+
+		return transactions;
 	}
 
 	static readTransactionFileLines(filePath) {
@@ -107,7 +123,7 @@ class XMLParser {
 			const rawTransactionsText = this.removeHeader(rawTransactionFile.split("\n"));
 			return rawTransactionsText;
 		} catch (err) {
-			console.error("Could not read the file! Check .\\logs\\debug.logs for details");
+			console.error("Could not read the file! Check .\\logs\\latest.logs for details");
 			logs.error(LogFormats.formatError(err, "Failed to read file!"));
 			return [];
 		}
